ad684e275caf8834b5d27ac35f05f7299ee52dd9
Antreas Kontopoulos <2349337K@student.gla.ac.uk>
2018-01-23 17:25:12 +0000

    chapter 3 - Mapping URLs

--- rango/urls.py
+++ rango/urls.py
@@ -1,5 +1,7 @@
-from django.conf.urls import patterns, url
+# from django.conf.urls import patterns, url
+from django.conf.urls import *
 from rango import views
 
-urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'))
+urlpatterns = [
+    url(r'^$', views.index, name='index'),
+]
